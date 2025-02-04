0037f75ca731385db648f7a5110d8721aeb797b7
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-05 01:54:22 +0600

    teach mode && customize

--- src/components/NavbarRoutes.tsx
+++ src/components/NavbarRoutes.tsx
@@ -16,10 +16,13 @@ export default function NavbarRoutes ()  {
     <div className='flex gap-x-2 ml-auto'>
       {
         isTeacherPage || isPlayerPage ? (
+            <Link href='/'>
+
             <Button size='sm' variant='ghost'>
               <LogOut className='h-4 w-4 mr-2'/>
               Exit
             </Button>
+              </Link>
         ) :(
             <Link href='/teacher/courses'>
               <Button size='sm' variant='ghost'>
