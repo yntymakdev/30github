f1559f25dbcbe71dc1a3e984fc2d1c0d3927632e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-05 03:32:14 +0600

    now i am really tired, let s build develop tomorrow

---
+++ src/app/(platform)/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+//   const formSchema = z.object({
+// title: z.string().min(1){
+//   message: "title is requires"
+//   }
+//   })
+export default function CreatePage()  {
+  // const form = useForm
+
+  return (
+    <div>
+    CreatePage
+  
+    </div>
+  );
+};
+
+
