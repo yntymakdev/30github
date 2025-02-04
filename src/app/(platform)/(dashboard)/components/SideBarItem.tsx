6206651db93bec1b6cef6e1221dc37b17e633552
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 22:18:25 +0600

    checking how it s look

--- src/app/(platform)/(dashboard)/components/SideBarItem.tsx
+++ src/app/(platform)/(dashboard)/components/SideBarItem.tsx
@@ -23,7 +23,11 @@ const onClick = () => {
   return (
     <button onClick={onClick} type='button' className={cn('flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:bg-slate-300/20', isActive && 'text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700')}>
     <div className='flex items-center gap-x-2 py-4'>
-      <Icon/>
+      <Icon
+      size={22}
+      className={cn('text-slate-500', isActive && 'text-sky-700')}
+      />
+      {label}
 
     </div>
   
