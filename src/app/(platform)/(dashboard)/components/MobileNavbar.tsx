291ef1d8a0bad7391f14a2f2ab8e680979444963
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 23:24:50 +0600

    custmozie mobileNavbar

--- src/app/(platform)/(dashboard)/components/MobileNavbar.tsx
+++ src/app/(platform)/(dashboard)/components/MobileNavbar.tsx
@@ -1,11 +1,18 @@
 'use client'
 import {Menu} from "lucide-react";
+import {Sheet, SheetTrigger} from "@/components/ui/sheet";
 
 export default function MobileNavbar ()  {
   return (
+
+      <Sheet>
+<SheetTrigger className="hidden md:flex h-full w-56 flex-col">
+
     <div>
     <Menu/>
     </div>
+</SheetTrigger>
+      </Sheet>
   );
 };
 
