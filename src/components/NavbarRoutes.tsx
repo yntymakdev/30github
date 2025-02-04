34cc3bebc74b497ddefcd090876f4353f6e75ea3
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-05 01:43:02 +0600

    navbar routes

--- src/components/NavbarRoutes.tsx
+++ src/components/NavbarRoutes.tsx
@@ -3,9 +3,10 @@ import {UserButton} from "@clerk/nextjs";
 import {usePathname, useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import {LogOut} from "lucide-react";
+import Link from "next/link";
 
 export default function NavbarRoutes ()  {
-  const  pathname = usePathname()
+  const  pathname = usePathname();
   const router = useRouter();
 
   const isTeacherPage = pathname.startsWith("/teacher");
@@ -19,14 +20,14 @@ export default function NavbarRoutes ()  {
               <LogOut className='h-4 w-4 mr-2'/>
               Exit
             </Button>
-
-        )
-
-      }
-
+        ) :(
+            <Link href='/teacher/courses'>
+              <Button>
+                Teach Mode
+              </Button>
+            </Link>
+      )}
       <UserButton/>
-
-
     </div>
   );
 };
