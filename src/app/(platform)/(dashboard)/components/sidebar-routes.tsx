5ff9120ed855af69fd92e5a12b057d7abc239f0d
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 21:38:57 +0600

    costumize a sidebar Routes

--- src/app/(platform)/(dashboard)/components/sidebar-routes.tsx
+++ src/app/(platform)/(dashboard)/components/sidebar-routes.tsx
@@ -1,7 +1,24 @@
 'use client'
+import {Compass, Layout} from "lucide-react";
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label: 'Dashboard',
+        href: '/',
+    } ,
+    {
+        icon: Compass,
+        label: 'Browse',
+        href: '/search',
+    }
+]
+
 export default function SidebarRoutes ()  {
+    const  router = guestRoutes;``
+
   return (
-    <div>
+    <div className='flex flex-col w-full'>
     sidebar-routes
   
     </div>
