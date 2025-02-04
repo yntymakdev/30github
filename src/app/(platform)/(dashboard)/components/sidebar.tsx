ca8e05780d0e6b1b59244dcca544c2727c17d276
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 21:15:11 +0600

    working !!! (hidden)

--- src/app/(platform)/(dashboard)/components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/components/sidebar.tsx
@@ -2,7 +2,7 @@
 
 export default function Sidebar ()  {
   return (
-    <div className='h-full'>
+    <div className='h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm'>
         SideBar !
 {/*<UserButton/>*/}
     </div>
