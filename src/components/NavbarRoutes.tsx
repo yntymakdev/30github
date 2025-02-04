d683b049d6e321b0a33c1ab1854bd7dfce08ac03
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 23:45:14 +0600

    customizing a navigation for chapter and teacher page

--- src/components/NavbarRoutes.tsx
+++ src/components/NavbarRoutes.tsx
@@ -1,11 +1,18 @@
 'use client'
 import {UserButton} from "@clerk/nextjs";
+import {usePathname, useRouter} from "next/navigation";
 
 export default function NavbarRoutes ()  {
+  const  pathname = usePathname()
+  const router = useRouter();
+
+  const isTeacherPage = pathname.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className='flex gap-x-2 ml-auto'>
       <UserButton/>
-    NavbarRoutes
+
 
     </div>
   );
