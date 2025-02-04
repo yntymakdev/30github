8c7ba7931a52fdb11ba0466bcf9d0d0c7a7889e0
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-05 02:42:07 +0600

    customize /teach-modeDashBoard/

--- src/app/(platform)/(dashboard)/components/SideBarRoutes.tsx
+++ src/app/(platform)/(dashboard)/components/SideBarRoutes.tsx
@@ -1,60 +1,64 @@
 'use client'
-import {Compass, Layout} from "lucide-react";
+import { Compass, Layout } from "lucide-react";
 import SideBarItem from "@/app/(platform)/(dashboard)/components/SideBarItem";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
     {
         icon: Layout,
         label: 'Dashboard',
         href: '/',
-    } ,
+    },
     {
         icon: Compass,
         label: 'Browse',
         href: '/search',
-    }, {
+    },
+    {
         icon: Compass,
         label: 'Settings',
         href: '/settings',
-    }, {
+    },
+    {
         icon: Compass,
         label: 'Think',
         href: '/think',
-    }, {
+    },
+    {
         icon: Compass,
         label: 'What',
         href: '/what',
     },
-],
-    const teacherRoutes = [
+];
+
+const teacherRoutes = [
     {
-        icon: Layout,
+        icon: List,
         label: 'Courses',
         href: '/teacher/courses',
-    } ,
+    },
     {
-        icon: Compass,
+        icon: BarChart,
         label: 'Analitycs',
         href: '/teacher/analitycs',
     },
-]
+];
 
-export default function SideBarRoutes ()  {
-    const  routes = guestRoutes;
+export default function SideBarRoutes() {
+    const pathname = usePathname();
+    const isTeacherPage = pathname.startsWith("/teacher"); // Предположим, что это определяет, является ли текущая страница страницей учителя
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
-  return (
-    <div className='flex flex-col w-full'>
-        {routes.map((route) => (
-            <SideBarItem
-            key={route.href}
-            icon={route.icon}
-            label={route.label}
-            href={route.href}
-            />
-
-        ))}
-    </div>
-  );
-};
-
-
+    return (
+        <div className='flex flex-col w-full'>
+            {routes.map((route) => (
+                <SideBarItem
+                    key={route.href}
+                    icon={route.icon}
+                    label={route.label}
+                    href={route.href}
+                />
+            ))}
+        </div>
+    );
+}
