886ab9dd26d63575a3df88d3a2eaeca32c92aca9
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 23:14:45 +0600

    changed layout for sidebar

--- src/app/(platform)/(dashboard)/layout.tsx
+++ src/app/(platform)/(dashboard)/layout.tsx
@@ -10,8 +10,10 @@ export default function DashBoardLayout({ children }: { children: ReactNode }) {
 <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
    <SideBar/>
 </div>
+<main className={'md:pl-56'}>
 
       {children}
+</main>
    </div>
   );
   }
