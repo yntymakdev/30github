7cf2dbf2d59503c2db854c3d49ae7ce695dbd50d
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 21:29:47 +0600

    added logo/costumize

--- src/app/(platform)/(dashboard)/components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/components/sidebar.tsx
@@ -1,10 +1,14 @@
-// import {UserButton} from "@clerk/nextjs";
+import Logo from "@/app/(platform)/(dashboard)/components/logo";
 
 export default function Sidebar ()  {
   return (
     <div className='h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm'>
+
+        <div className='p-6'>
+            <Logo/>
+
+        </div>
         SideBar !
-{/*<UserButton/>*/}
     </div>
   );
 };
