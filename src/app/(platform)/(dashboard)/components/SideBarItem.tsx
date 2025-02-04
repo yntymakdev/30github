b3c50529b1c1d58d9f88770c3931a4b0305eaafa
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-04 22:16:29 +0600

    adding style for sidebaritem

--- src/app/(platform)/(dashboard)/components/SideBarItem.tsx
+++ src/app/(platform)/(dashboard)/components/SideBarItem.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import {LucideIcon} from "lucide-react";
+import {usePathname, useRouter} from "next/navigation";
+import {cn} from "@/lib/utils";
 
 interface  SideBarItemProps {
   icon: LucideIcon,
@@ -9,12 +11,23 @@ interface  SideBarItemProps {
 
 }
 
-export default function SideBarItem ({icon,label,href}: SideBarItemProps)  {
+export default function SideBarItem ({icon: Icon,label,href}: SideBarItemProps)  {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isActive = (pathname === '/' && href === '/') || pathname?.startsWith(`${href}/`);
+const onClick = () => {
+  router.push(href);
+}
+
   return (
-    <div>
-    SideBarItem
-  
+    <button onClick={onClick} type='button' className={cn('flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:bg-slate-300/20', isActive && 'text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700')}>
+    <div className='flex items-center gap-x-2 py-4'>
+      <Icon/>
+
     </div>
+  
+    </button>
   );
 };
 
