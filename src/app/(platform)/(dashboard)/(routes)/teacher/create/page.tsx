5fa653c8e08d19b838c8c1bf7987f9392313c12e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-05 13:36:00 +0600

    now finished form

--- src/app/(platform)/(dashboard)/(routes)/teacher/create/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/create/page.tsx
@@ -5,7 +5,7 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
 const formSchema = z.object({
@@ -20,7 +20,7 @@ export default function CreatePage() {
         }
     });
 
-    const {isValid, isSubmitting } = form.formState;
+    const { isValid, isSubmitting, errors } = form.formState;
 
     const onSubmit = (values: z.infer<typeof formSchema>) => {
         console.log(values);
@@ -29,43 +29,45 @@ export default function CreatePage() {
     return (
         <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6'>
             <div>
-                <h1 className='text-2xl'>Name your course   </h1>
+                <h1 className='text-2xl'>Name your course</h1>
                 <p className='text-sm text-slate-600'>
-                    What would you like to name yout course?Dont worry you can change later
+                    What would you like to name your course? Don't worry, you can change it later.
                 </p>
-            <Form {...form}>
-                <form className='space-y-8 mt-8' onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        control={form.control}
-                        name='title'
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Course title</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        disabled={isSubmitting}
-                                        placeholder='e.g Advanced web development'
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>What will you teach in this course?</FormDescription>
-                    <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
 
-                    <div className='flex items-center gap-x-2'>
-                        <Link href='/'>
-                            <Button type='button' variant='ghost'>
-                                Cancel
+                <FormProvider {...form}>
+                    <form className='space-y-8 mt-8' onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name='title'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Course title</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder='e.g Advanced web development'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>What will you teach in this course?</FormDescription>
+                                    {errors.title && <FormMessage>{errors.title.message}</FormMessage>}
+                                </FormItem>
+                            )}
+                        />
+
+                        <div className='flex items-center gap-x-2'>
+                            <Link href='/'>
+                                <Button type='button' variant='ghost'>
+                                    Cancel
+                                </Button>
+                            </Link>
+                            <Button type='submit' disabled={!isValid || isSubmitting}>
+                                Continue
                             </Button>
-                        </Link>
-                        <Button type='submit' disabled={!isValid || isSubmitting}>
-                            Continue
-                        </Button>
+                        </div>
+                    </form>
+                </FormProvider>
+            </div>
         </div>
-                </form>
-</Form>
-</div>
     );
-};
+}
