c5ad285be98c04643879d2e2045103ef84376710
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-06 21:03:14 +0600

    writing style for edit page onsidebar tech mode

--- src/components/icon-badge.tsx
+++ src/components/icon-badge.tsx
@@ -1,3 +1,5 @@
+import {cva, type VariantProps} from "class-variance-authority";
+
 const  backgroundVariants= cva(
     'rounded-full flex items-center justify-center',
     {variants: {
@@ -11,9 +13,38 @@ const  backgroundVariants= cva(
             },
             size: {
             default: 'p-2',
-            }
-
-
-        }}
-
+                sm: 'p-1',
+            },
+        },
+    defaultVarianst: {
+        variant: 'default',
+        size: 'default',
+    },
+        },
 )
+const   iconVariants = cva(
+    '',{
+        variants: {
+            variant: {
+                default: 'text-sky-700',
+                success: 'text-emerald-700',
+            },
+            size: {
+                default: 'p-2',
+                sm: 'p-1',
+            },
+        }
+        defaultVarianst: {
+            variant: 'default',
+            size: 'default',
+        }
+
+    }
+)
+
+type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
+type IconVariantsProps = VariantProps<typeof iconVariants>;
+
+
+
+
