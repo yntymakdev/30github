295f1efaee8c420b28e83b9f115d08523e8014ac
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-06 23:44:35 +0600

    style writing

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -2,10 +2,11 @@ export async function PATCH(
 req:Request
 ){
     try {
-
+const {userId} = await  auth()
+        const {courseId} = await  auth()
     }
     catch (){
-
+        console.log()
     }
 
 
