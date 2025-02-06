e9a6359489464c46528ccd8152b1239f3af9e00e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-06 23:45:30 +0600

    style writing

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -1,10 +1,16 @@
+import {NextResponse} from "next/server";
+
 export async function PATCH(
 req:Request
 ){
     try {
 const {userId} = await  auth()
         const {courseId} = await  auth()
+        if(!userId){
+            return new NextResponse('Unauthorized', {status: 401})
+        }
     }
+    catch(err){}
     catch (){
         console.log()
     }
