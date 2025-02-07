185ce84a3174416646496d840f3d8bbc65ca8296
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-07 11:45:39 +0600

    async server customize

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -3,21 +3,23 @@ import {NextResponse} from "next/server";
 
 export async function PATCH(
 req:Request
+{params}: {params: {courseId: string}}
+
 ){ 
     try {
-const {userId} = await  auth()
-const {courseId} = params
+        const {userId} = await  auth()
+        const {courseId} = params
+        const values = await req.json()
 if(!userId){
     return new NextResponse('Unauthorized', {status: 401})
         }
     }
-
-if(!userId){
-return new NextResponse('Unauthorized', {status: 401})
-
-}
-
-    const course = await  db.course.update
+    
+    if(!userId){
+        return new NextResponse('Unauthorized', {status: 401})
+       
+    }
+        const course = await  db.course.update
     catch (){
         console.log()
     }
