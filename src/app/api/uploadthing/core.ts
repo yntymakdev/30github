b851afa795593718b65e43120ddb17739310b573
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-09 09:09:31 +0600

    customize file upload page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,29 +3,26 @@ import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" });
+const auth = (req: Request) => ({ userId: "fakeId" });
+
+const handleAuth = (req: Request) => {
+    const { userId } = auth(req);
+    if (!userId) throw new UploadThingError("Unauthorized");
+    return { userId };
+};
 
 export const ourFileRouter = {
-    imageUploader: f({
-        image: {
-            maxFileSize: "4MB",
-            maxFileCount: 1,
-        },
-    })
-        .middleware(async ({ req }) => {
-            const user = await auth(req);
+    courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(({ req }) => handleAuth(req))
+        .onUploadComplete(() => {}),
 
-            if (!user) throw new UploadThingError("Unauthorized");
+    courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+        .middleware(({ req }) => handleAuth(req))
+        .onUploadComplete(() => {}),
 
-            return { userId: user.id };
-        })
-        .onUploadComplete(async ({ metadata, file }) => {
-            console.log("Upload complete for userId:", metadata.userId);
-
-            console.log("file url", file.url);
-
-            return { uploadedBy: metadata.userId };
-        }),
+    chapterVideo: f({ video: { maxFileSize: "2GB", maxFileCount: 1 } }) // Ограничил до 2GB
+        .middleware(({ req }) => handleAuth(req))
+        .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
