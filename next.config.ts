d9c01af2b167e228f6efd940ba8e88a80c715e46
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-09 22:14:35 +0600

    now i am fixed that bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: 'null-loader',
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
