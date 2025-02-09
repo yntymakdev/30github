1bd78ec9a74575cd21c3b222e6cae73010f16266
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-09 23:10:19 +0600

    new ui compoenents

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ImageForm.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ImageForm.tsx
@@ -66,7 +66,7 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
                 </div>
             ) : (
                 <div className='relative aspect-video mt-2'>
-                    <Image width={100} height={100} alt='Upload' className='object-cover rounded-md' src={initialData.imageUrl}/>
+                    <Image fill alt='Upload' className='object-cover rounded-md' src={initialData.imageUrl}/>
 
                 </div>
             )
