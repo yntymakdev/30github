5543fbf3af4b4dee23788449fa13e437bcb9e91e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-09 23:36:27 +0600

    changing shadcn ui components

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -57,7 +57,7 @@ export const ComboboxDemo = ({
                                     <Check
                                         className={cn(
                                             "mr-2 h-4 w-4",
-                                            value === framework.value ? "opacity-100" : "opacity-0"
+                                            value === option.value ? "opacity-100" : "opacity-0"
                                         )}
                                     />
                                     {framework.label}
