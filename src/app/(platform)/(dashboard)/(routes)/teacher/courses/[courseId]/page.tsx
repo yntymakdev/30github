65e74e8f7efc91bab1e1fbbdfce440983ccced25
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-09 23:51:29 +0600

    new feature

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -23,7 +23,7 @@ const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) =>
         return redirect("/");
     }
 
-    const ca
+    const categories
 
     const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId];
     const totalFields = requiredFields.length;
