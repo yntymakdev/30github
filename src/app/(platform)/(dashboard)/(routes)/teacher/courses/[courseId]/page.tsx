c19f07be3fe39394b0b0497dc47330a90b2c6047
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-10 21:20:23 +0600

    customize a next feature

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,11 +2,11 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
-import {LayoutDashboard, ListChecks} from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { TitleForm } from "./components/TitleForm";
 import { DescriptionForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/DescriptionForm";
-import {ImageForm} from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ImageForm";
-import {CategoryForm} from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/CategoryForm";
+import { ImageForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ImageForm";
+import { CategoryForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/CategoryForm";
 
 const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = await auth();
@@ -15,27 +15,16 @@ const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) =>
     }
 
     const course = await db.course.findUnique({
-        where: {
-            id: params.courseId,
-        },
+        where: { id: params.courseId },
     });
 
-    const categories = await db.category.findMany({
-        orderBy:{
-            name: 'asc',
-        }
-    })
-    console.log(categories)
-    if(!course){
+    const categories = await db.category.findMany({ orderBy: { name: "asc" } });
+
+    if (!course) {
         return redirect("/");
     }
 
-    const requiredFields = [
-        course.title,
-        course.description,
-        course.imageUrl,
-        course.price,
-        course.categoryId];
+    const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId];
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
     const completionText = `(${completedFields}/${totalFields})`;
@@ -48,27 +37,18 @@ const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) =>
                     <span className="text-sm text-slate-700">Completed fields {completionText}</span>
                 </div>
             </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                 <div>
                     <div className="flex items-center gap-x-2">
                         <IconBadge icon={LayoutDashboard} />
                         <h2 className="text-xl">Customize your course</h2>
                     </div>
-                    <TitleForm
-                        initialData={{
-                            title: course.title ?? "",  // Ensure title is not null
-                        }}
-                        courseId={course.id}
-                    />
-                    <DescriptionForm
-                        initialData={{
-                            description: course.description ?? "",  // Ensure description is not null
-                        }}
-                        courseId={course.id}
-                    /> <ImageForm
-                        initialData={course}
-                        courseId={course.id}
-                    /><CategoryForm
+
+                    <TitleForm initialData={{ title: course.title ?? "" }} courseId={course.id} />
+                    <DescriptionForm initialData={{ description: course.description ?? "" }} courseId={course.id} />
+                    <ImageForm initialData={course} courseId={course.id} />
+                    <CategoryForm
                         initialData={course}
                         courseId={course.id}
                         options={categories.map((category) => ({
@@ -77,21 +57,21 @@ const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) =>
                         }))}
                     />
                 </div>
-<div className='space-y-6'>
 
+                <div className="space-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={ListChecks} />
+                            <h2 className="text-xl">Course chapters</h2>
+                        </div>
+                        <p>TODO: Chapters</p>
+                    </div>
 
-                <div className='flex items-center gap-x-2'>
-                    <IconBadge icon={ListChecks} />
-                    <h2 className='text-xl '>
-                        Course chapters
-                    </h2>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={CircleDollarSign} />
+                        <h2 className="text-xl">Sell your course</h2>
+                    </div>
                 </div>
-</div>
-                <div>
-
-                </div>
-
-
             </div>
         </div>
     );
