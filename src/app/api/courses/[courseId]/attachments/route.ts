b7f24df8cb5121f6dbee90d6d7084516663e7155
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-12 17:17:58 +0600

    customixe route ts for attachemnt

--- src/app/api/courses/[courseId]/attachments/route.ts
+++ src/app/api/courses/[courseId]/attachments/route.ts
@@ -1,31 +1,37 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
+import {auth} from "@clerk/nextjs/server";
+import {db} from "@/lib/db";
 
-export async function POST(
-    req:Request,
-        {params}: {params: courseId:string}
-){
+export async function POST(req: Request, { params }: { params: { courseId: string } }) {
     try {
-const {userId} = auth();
-const {url} = await  req.json()
-        if(!userId){
-return new NextResponse('Unauthorized', {status:401});
-        }
-        const  courseOwner = await  db.course.findUnique({where:{id:params.courseId}});
-        if(!courseOwner){
-            return new NextResponse('Unautorized', {status:401})
+        const { userId } =  await auth();
+        const { url } = await req.json();
+
+        if (!userId) {
+            return new NextResponse('Unauthorized', { status: 401 });
         }
 
-        const attachment = await  db.attachment.create({
-            data:{
+        const courseOwner = await db.course.findUnique({ where: { id: params.courseId } });
+        if (!courseOwner) {
+            return new NextResponse('Not Found', { status: 404 });  // изменен статус с 401 на 404
+        }
+
+        const fileName = url.split('/').pop();
+        if (!fileName) {
+            return new NextResponse('Invalid file URL', { status: 400 });
+        }
+
+        const attachment = await db.attachment.create({
+            data: {
                 url,
-                name:url.split('/').pop(),
-                courseId:params.courseId
+                name: fileName,
+                courseId: params.courseId,
             }
-        })
+        });
 
         return NextResponse.json(attachment);
-    }catch(err){
-        console.log('COURSE_ID_ATTACHEMENTS', error);
-        return new NextResponse('Internal server error',{status:500});
+    } catch (err) {
+        console.log('COURSE_ID_ATTACHEMENTS', err);
+        return new NextResponse('Internal server error', { status: 500 });
     }
 }
