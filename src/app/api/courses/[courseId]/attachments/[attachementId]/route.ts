c2ae621d7e8def007b5759204287bc3e813da30a
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-13 08:05:38 +0600

    creating next api route for attachemntId

--- src/app/api/courses/[courseId]/attachments/[attachementId]/route.ts
+++ src/app/api/courses/[courseId]/attachments/[attachementId]/route.ts
@@ -1,4 +1,26 @@
+import {NextResponse} from "next/server";
+import {auth} from "@clerk/nextjs/server";
+import {db} from "@/lib/db";
+
 export async function DELETE(
     req: Request,
     {params}: {params: {courseId:string,attachmentId:string}}
-)
+){
+    try {
+const {userId} = await  auth()
+        return new NextResponse('Unauthorized', {status: 401})
+    }
+
+    const courseOwner = await  db.course.findUnique({
+        where:{
+            id: params.courseId,
+            userId:userId
+
+        }
+    })
+
+    catch (error) {
+        console.error('ATTACHMENT_ID', error);
+        return new NextResponse('Internal server error', {status:500});
+    }
+}
