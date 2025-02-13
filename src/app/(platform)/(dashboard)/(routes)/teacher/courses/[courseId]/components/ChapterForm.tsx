77166635dd7bdab02b4ecfb14cf78da5ff250f4d
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-14 00:36:43 +0600

    customize chapter route ts

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from "react";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
---
+++ src/app/api/courses/[courseId]/chapters/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import {auth} from "@clerk/nextjs/server";
+import {db} from "@/lib/db";
+
+export async function POST(req: Request, { params }: { params: { courseId: string } }) {
+  try {
+    const { userId } =  await auth();
+    const { url } = await req.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({ where: { id: params.courseId } });
+    if (!courseOwner) {
+      return new NextResponse('Not Found', { status: 404 });
+    }
+
+    const fileName = url.split('/').pop();
+    if (!fileName) {
+      return new NextResponse('Invalid file URL', { status: 400 });
+    }
+
+    const attachment = await db.attachment.create({
+      data: {
+        url,
+        name: fileName,
+        courseId: params.courseId,
+      }
+    });
+    return NextResponse.json(attachment);
+  } catch (err) {
+    console.log('COURSE_ID_ATTACHMENTS', err);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
