3c0b144e526e5dcd9ab7b61bb529900373946579
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-14 23:24:19 +0600

    customize chapterlist page

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -8,6 +8,8 @@ onReorder: (updateData: { id:string;position:number }[]) => void;
 onEdit: (id:string) => void;
 }
 export default function ChapterList ({items,onReorder,onEdit}:ChapterListFrom)  {
+  const
+
   return (
     <div>
     ChapterList
