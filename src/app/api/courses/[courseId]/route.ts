a0385793e24be18efbfdb9a3369d18e66388f92b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-14 16:25:05 +0600

    customixe fixing bug

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -11,21 +11,19 @@ export async function PATCH(req: Request, { params }: { params: { courseId: stri
             return new NextResponse("Unauthorized", { status: 401 });
         }
 
-        // Parse the request body to get the updated course data
         const values = await req.json();
 
-        // Update the course in the database
         const course = await db.course.update({
             where: {
                 id: courseId,
-                userId, // Ensure the course is associated with the authenticated user
+                userId,
             },
             data: {
-                ...values, // Update with the new values passed from the request
+                ...values,
             },
         });
 
-        return NextResponse.json(course); // Return the updated course data
+        return NextResponse.json(course);
     } catch (error) {
         console.log("[COURSE_ID]", error);
         return new NextResponse("Internal server error", { status: 500 });
