1da4d96472f5ac359d692d767e93c54079541d0f
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-15 20:26:51 +0600

    customize chapterlist drap and drop

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -52,7 +52,7 @@ export default function ChapterList ({items,onReorder,onEdit}:ChapterListFrom)  
 
                            ><Grid className='h-5 w-5'/>
                            </div>
-                          {}
+                          {chap}
 
                         </div>
 
