d3a213c464f31cda1c07dcc63bc251ce424628a1
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-15 20:26:12 +0600

    customize chapterlist drap and drop

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -49,7 +49,7 @@ export default function ChapterList ({items,onReorder,onEdit}:ChapterListFrom)  
 
                                 {...provided.dragHandleProps}
 
-                           ><Gr
+                           ><Gri
 
                            </div>
 
