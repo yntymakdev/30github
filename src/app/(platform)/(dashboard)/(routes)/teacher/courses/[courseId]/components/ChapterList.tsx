ea2a7fb15765be72c0bb56299364b6dcc5815c9b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-16 22:46:06 +0600

    customize chapterlist drap and drop

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -61,7 +61,7 @@ Free
                             )}
                             <Badge className={cn(
                                 'bg-slate-500',
-                                chapter.isPublished?"Publish"
+                                chapter.isPublished?"Publishe"
                             )}></Badge>
                           </div>
 
