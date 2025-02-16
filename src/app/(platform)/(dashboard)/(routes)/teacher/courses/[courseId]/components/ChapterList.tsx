4d7422017dec3265c13b2c6aabd57537fdca207c
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-16 22:42:48 +0600

    customize chapterlist drap and drop

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -59,7 +59,7 @@ export default function ChapterList ({items,onReorder,onEdit}:ChapterListFrom)  
 Free
                                 </Badge>
                             )}
-                            <Bad
+                            <Badge
                           </div>
 
                         </div>
