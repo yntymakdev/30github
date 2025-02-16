1efb60ee8d419479720825860cda2dfa440ece00
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-16 22:42:32 +0600

    now it will so fast

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterList.tsx
@@ -59,7 +59,7 @@ export default function ChapterList ({items,onReorder,onEdit}:ChapterListFrom)  
 Free
                                 </Badge>
                             )}
-                            <
+                            <B
                           </div>
 
                         </div>
