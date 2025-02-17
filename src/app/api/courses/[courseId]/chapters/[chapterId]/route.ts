4be2ff0608897c583c16ca5432eabfeab3c8e28f
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 00:30:30 +0600

    inside chapterId route ts for chapter

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -7,8 +8,32 @@ export async function PATCH(req: Request
     try {
 const {userId} = await auth()
 const values  = await req.json()
-if(!userId)         
-    } catch (error) {
+if(!userId)  {
+    return new NextResponse('Unautorized', {status:401})
+}       
+    } 
+    const ownCourse  = await db.course.findUnique({
+        where: {    
+
+            id:params.courseId,
+            userId
+        }
+    })
+    if(!ownCourse)  {
+        return new NextResponse('Unautorized', {status:401})
+    }       
+const chapter = await db.chapter.update({
+    where: {
+        id:params.chapterId,
+        courseId:params.courseId
+    },
+    data:{
+        ...values
+    }
+})
+
+}
+    catch (error) {
      console.log("[COURSES_CHAPTER_ID]", error);
      return  new NextResponse('Internal server error', {status: 500})
         
