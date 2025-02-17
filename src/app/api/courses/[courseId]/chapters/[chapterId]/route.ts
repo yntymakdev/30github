0f876794a2d2c1f24d10963f6a10427d5e9841ba
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 00:55:53 +0600

    fixing some bug on chapterId page inside route ts for api

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function PATCH(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
   try {
     const { userId } = await auth();
-    const values = await req.json();
+    const { isPublished, ...values } = await req.json();
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
