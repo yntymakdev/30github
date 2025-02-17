b133b63bcffa14b10c0048c70c83fb406a2a5444
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-17 21:49:32 +0600

    customize chapterId page edit

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,55 +1,45 @@
-import React from 'react';
-import {auth} from "@clerk/nextjs/server";
-import {redirect} from "next/navigation";
-import {db} from "@/lib/db";
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 import Link from "next/link";
 
+const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId: string } }) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
+  if (!chapter) {
+    return redirect("/");
+  }
 
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
 
-
-const ChapterIdPage =async ({params}: {params:{courseId:string;chapterId:string}}) => {
-
- const {userId} = await  auth()
-    if(!userId){
-        return redirect('/')
-    }
-    const chapter = await  db.chapter.findUnique({
-        where: {
-            id: params.chapterId,
-            courseId: params.courseId
-        },
-        include:{
-            muxData: true
-        }
-    })
-    if(!chapter){
-
-        return redirect('/')
-    }
-
-    const requiredFields = [
-        chapter.title,
-        chapter.description,
-        chapter.videoUrl,
-    ]
-
-    const totalFields = requiredFields.length;
-    const completedFields = requiredFields.filter(Boolean).length;
-    const completionText = (`${completedFields}/${totalFields}`)
-    return (
-        <div className='p-6'>
-            <div className='flex items-center justify-center'>
-                <div className='w-full'>
-                    <Link href={`/teacher/courses/${params.courseId}`}
-                     className=''
-                    />
-
-                </div>
-
-            </div>
-          Chapterid
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `${completedFields}/${totalFields}`;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-center">
+        <div className="w-full">
+          <Link
+            href={`/teacher/courses/${params.courseId}`}
+            className="flex items-center text-sm hover:opacity-75 transition mb-6"
+          />
         </div>
-    );
+      </div>
+      Chapterid
+    </div>
+  );
 };
 
 export default ChapterIdPage;
