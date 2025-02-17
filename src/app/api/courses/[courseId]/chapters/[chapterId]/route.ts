32413d90c4e7e42efdb7ab67a6a1037c5662cc00
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 00:21:18 +0600

    inside chapterId route ts for chapter

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,10 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function PATCH(req: Request
 {params}: {params: {courseId:string;chapterId:string}}){
 
     try {
-const {userId} =         
+const {userId} = await auth()
+const values  = await req.json()
+if(!userId)         
     } catch (error) {
      console.log("[COURSES_CHAPTER_ID]", error);
      return  new NextResponse('Internal server error', {status: 500})
