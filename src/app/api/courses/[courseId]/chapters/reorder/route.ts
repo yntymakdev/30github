046a549c6452c4590712850d7f341a40a709c330
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-17 15:50:51 +0600

    creating route api for reorder on chapter

--- src/app/api/courses/[courseId]/chapters/reorder/route.ts
+++ src/app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -1,9 +1,13 @@
+import {auth} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
+
 export async function PUT(
     req: Request,{params}: {params: {courseId: string}}
 ){
     try {
-
+const {userId} = await auth()
     }catch (error){
-        console.log(error)
+        console.log('[REORDER]', error);
+        return new NextResponse('Internal Server Error', {status: 500})
     }
 }
