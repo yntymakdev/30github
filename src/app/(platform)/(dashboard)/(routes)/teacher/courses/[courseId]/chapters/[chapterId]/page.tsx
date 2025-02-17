d8c62590c536e6c488007d7a733eecd41184ad3c
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-17 21:33:25 +0600

    now i am gonna start edit page on ChaperForm

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import {db} from "@/lib/db";
 
-const ChapterIdPage = () => {
+
+
+
+const ChapterIdPage =async ({params}: {params:{courseId:string;chapterId:string}}) => {
+
+ const {userId} = await  auth()
+    if(!userId){
+        return redirect('/')
+    }
+    const chapter = await  db.chapter.findUnique({
+        where: {
+            id: params.chapterId,
+            courseId: params.courseId
+        },
+        include:{
+            muxData: true
+        }
+
+    })
+
     return (
         <div>
           Chapterid
