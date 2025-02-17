f9ef6213cd7131a277f6a38c077e0184ce7e75e0
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-17 21:01:17 +0600

    customize chapter form (if rendering)

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm.tsx
@@ -69,7 +69,7 @@
             <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
                 {isUpdating && (
                     <div className='absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center'>
-    <Loader2 className='animate-spin h-6 w-6 text-sky-700'/>
+    <Loader2 className='animate-spin h-10 w-10 text-sky-700'/>
                     </div>
                 )}
                 <div className="font-medium flex items-center justify-between">
