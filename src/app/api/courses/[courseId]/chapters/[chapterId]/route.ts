355027199ade0510c6d73d51ffc7761096db773b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 00:54:15 +0600

    fixing some bug on chapterId page inside route ts for api

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -2,40 +2,39 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request
-{params}: {params: {courseId:string;chapterId:string}}){
+export async function PATCH(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
+  try {
+    const { userId } = await auth();
+    const values = await req.json();
 
-    try {
-const {userId} = await auth()
-const values  = await req.json()
-if(!userId)  {
-    return new NextResponse('Unautorized', {status:401})
-}       
-    } 
-    const ownCourse  = await db.course.findUnique({
-        where: {    
-
-            id:params.courseId,
-            userId
-        }
-    })
-    if(!ownCourse)  {
-        return new NextResponse('Unautorized', {status:401})
-    }       
-const chapter = await db.chapter.update({
-    where: {
-        id:params.chapterId,
-        courseId:params.courseId
-    },
-    data:{
-        ...values
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-})
 
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.error("[COURSES_CHAPTER_ID]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
 }
-    catch (error) {
-     console.log("[COURSES_CHAPTER_ID]", error);
-     return  new NextResponse('Internal server error', {status: 500})
-        
-    }
-};
