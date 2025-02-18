83b8149f9a273391e94bbbe648bf69c3104d85cb
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 14:09:01 +0600

    preview page

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
+import ReactQuill from "react-quill";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -12,4 +13,10 @@ export const Edtor =
   () =>
   ({ onChange, value }: EditorProps) => {
     const ReactQuil = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+
+    return (
+      <div className="bg-white">
+        <ReactQuill theme="show" value={value} onChange={onChange} />
+      </div>
+    );
   };
