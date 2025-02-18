eb56d851f6c2ca590f397996dab7b775718909ff
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 14:03:09 +0600

    as

---
+++ src/components/editor.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useMemo } from "react";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+export const Edtor = () => ({
+  onChange,
+  value,
+}:EditorProps) => {
+    const   ReactQuil = useMemo((=> ))
+};
