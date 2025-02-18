0279e306efbad8b689fc3478b84bb58fb258d42b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-18 21:02:47 +0600

    fixing bug

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useMemo, useCallback } from "react";
-import { EditorState, ContentState } from "draft-js";
-import { Editor as DraftEditor } from "draft-js";
+import { useMemo } from "react";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -11,26 +9,10 @@ interface EditorProps {
 }
 
 export const Editor = ({ onChange, value }: EditorProps) => {
-  const [editorState, setEditorState] = useMemo(() => {
-    const initialState = value
-      ? EditorState.createWithContent(ContentState.createFromText(value))
-      : EditorState.createEmpty();
-    return [initialState, setEditorState] as const;
-  }, [value]);
-
-  const handleChange = useCallback(
-    (newEditorState: EditorState) => {
-      setEditorState(newEditorState);
-      const currentContent = newEditorState.getCurrentContent();
-      const currentText = currentContent.getPlainText();
-      onChange(currentText); // передаем обновленный текст родительскому компоненту
-    },
-    [onChange]
-  );
-
+  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
   return (
     <div className="bg-white">
-      <DraftEditor editorState={editorState} onChange={handleChange} placeholder="Start typing..." />
+      <ReactQuill theme="snow" value={value} onChange={onChange} />
     </div>
   );
 };
