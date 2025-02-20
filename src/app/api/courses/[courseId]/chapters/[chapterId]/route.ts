706b51d72b19f6916211fdc9d442a5010d10e790
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-20 23:06:19 +0600

    just starting again

--- src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,53 +1,63 @@
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import Mux from "@mux/mux-node";
-const { Video } = new Mux(process.env.MUX_TOKEN_ID!, process.env.MUX_TOKEN_SECRET!);
+  import { db } from "@/lib/db";
+  import { auth } from "@clerk/nextjs/server";
+  import { NextResponse } from "next/server";
+  import Mux from "@mux/mux-node";
+  const { Video } = new Mux(process.env.MUX_TOKEN_ID!, process.env.MUX_TOKEN_SECRET!);
+      
+  export async function PATCH(req: Request, { par ams }: { params: { courseId: string; chapterId: string } }) {
+    try {
+      const { userId } = await auth();
+      const { isPublished, ...values } = await req.json();
 
-export async function PATCH(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
-  try {
-    const { userId } = await auth();
-    const { isPublished, ...values } = await req.json();
+      if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+      }
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const ownCourse = await db.course.findUnique({
-      where: {
-        id: params.courseId,
-        userId,
-      },
-    });
-
-    if (!ownCourse) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const chapter = await db.chapter.update({
-      where: {
-        id: params.chapterId,
-        courseId: params.courseId,
-      },
-      data: {
-        ...values,
-      },
-    });
-    if (values.videoUrl) {
-      const existingMuxData = await db.muxData.findFirst({
+      const ownCourse = await db.course.findUnique({
         where: {
-          chapterId: params.chapterId,
+          id: params.courseId,
+          userId,
         },
       });
 
-      if (existingMuxData) {
-        await Video.Assets.del(existingMuxData.assetId);
+      if (!ownCourse) {
+        return new NextResponse("Unauthorized", { status: 401 });
       }
-    }
 
-    return NextResponse.json(chapter);
-  } catch (error) {
-    console.error("[COURSES_CHAPTER_ID]", error);
-    return new NextResponse("Internal server error", { status: 500 });
-  }
+      const chapter = await db.chapter.update({
+        where: {
+          id: params.chapterId,
+          courseId: params.courseId,
+        },
+        data: {
+          ...values,
+        },
+      });
+      if (values.videoUrl) {
+        const existingMuxData = await db.muxData.findFirst({
+          where: {
+            chapterId: params.chapterId,
+          },
+        });
+
+        if (existingMuxData) {
+
+          await Video.Assets.del(existingMuxData.assetId);
+          await db.muxData.delete({
+where: {
+  id:existingMuxData.id 
 }
+
+          })
+        }
+
+const asset = 
+
+      }
+
+      return NextResponse.json(chapter);
+    } catch (error) {
+      console.error("[COURSES_CHAPTER_ID]", error);
+      return new NextResponse("Internal server error", { status: 500 });
+    }
+  }
