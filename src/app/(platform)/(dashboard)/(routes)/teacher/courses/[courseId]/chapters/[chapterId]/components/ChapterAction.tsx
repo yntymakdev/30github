a8adb0cabfad970b1499bbab3b164c552a1dd656
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 22:22:09 +0600

    chapter actions creating

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/components/ChapterAction.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/components/ChapterAction.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 import React from "react";
 
 interface ChapterActionProps {
@@ -14,6 +15,9 @@ const ChapterAction = ({ disabled, courseId, chapterId, isPublished }: ChapterAc
     <div className="flex items-center gap-x-2">
       <Button onClick={() => {}} disabled={disabled} variant="outline" size="sm">
         {isPublished ? "Unpublished" : "Publish"}
+      </Button>
+      <Button size="sm">
+        <Trash className="h-4 w-4" />
       </Button>
     </div>
   );
