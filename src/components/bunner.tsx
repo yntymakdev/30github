cbf50e51d461039a57dcfc9d5c255087deaddc65
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 19:22:05 +0600

    ok custmoize bunner

--- src/components/bunner.tsx
+++ src/components/bunner.tsx
@@ -1,8 +1,11 @@
 import { cva } from "class-variance-authority";
 import React from "react";
 
-const bannerVariants = cva("bord text-center p-4 text-sm flex items-center w-full");
-
+const bannerVariants = cva("bord text-center p-4 text-sm flex items-center w-full", {
+  variants: {
+    variant: { warning: "bg-yellow-200/80 border-yellow-30 text-primary", success: "bg-emerald-700 border-emerald" },
+  },
+});
 const Banner = () => {
   return <div>Banner</div>;
 };
