6a23406c0640d7cf9265032bb7bbad180ee6acc2
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 19:23:00 +0600

    ok custmoize bunner

--- src/components/bunner.tsx
+++ src/components/bunner.tsx
@@ -3,8 +3,12 @@ import React from "react";
 
 const bannerVariants = cva("bord text-center p-4 text-sm flex items-center w-full", {
   variants: {
-    variant: { warning: "bg-yellow-200/80 border-yellow-30 text-primary", success: "bg-emerald-700 border-emerald" },
+    variant: {
+      warning: "bg-yellow-200/80 border-yellow-30 text-primary",
+      success: "bg-emerald-700 border-emerald-800 text-secondary",
+    },
   },
+  defaultVariants: { variant: "warning" },
 });
 const Banner = () => {
   return <div>Banner</div>;
