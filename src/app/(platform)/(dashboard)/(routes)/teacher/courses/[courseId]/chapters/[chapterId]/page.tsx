9899dac8f564638ec6ebbf58047bf9b2a48bd0f1
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 20:51:38 +0600

    customuxe bunner tsx

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -36,49 +36,51 @@ const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId
   const completionText = `${completedFields}/${totalFields}`;
 
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-center">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${params.courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="h-4 w-4 mr-2" />
-            Back to course setup
-          </Link>
-          <div className="flex items-center justify-between w-fill">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">Chapter Creation</h1>
-              <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-center">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Back to course setup
+            </Link>
+            <div className="flex items-center justify-between w-fill">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Chapter Creation</h1>
+                <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Customize your chapter</h2>
+              </div>
+              <ChapterTitleForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
+              <ChapterDescriptionForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Eye} />
+                <h2 className="text-xl">Access Settings</h2>
+              </div>
+              <ChapterAccessForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
+            </div>
+          </div>
           <div>
             <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">Customize your chapter</h2>
+              <IconBadge icon={Video} />
+              <h2 className="text-xl">Add a video</h2>
             </div>
-            <ChapterTitleForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
-            <ChapterDescriptionForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={Eye} />
-              <h2 className="text-xl">Access Settings</h2>
-            </div>
-            <ChapterAccessForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
+            <ChapterVideoForm courseId={params.courseId} chapterId={params.chapterId} initialData={chapter} />
           </div>
-        </div>
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={Video} />
-            <h2 className="text-xl">Add a video</h2>
-          </div>
-          <ChapterVideoForm courseId={params.courseId} chapterId={params.chapterId} initialData={chapter} />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
