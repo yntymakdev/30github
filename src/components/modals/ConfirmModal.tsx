70fb713d8b321f8dacebbc3143f4d6ca25e4bdb5
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 22:45:53 +0600

    customixe confirm model

--- src/components/modals/ConfirmModal.tsx
+++ src/components/modals/ConfirmModal.tsx
@@ -1,13 +1,24 @@
+import { AlertDialog, AlertDialogContent, AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
+import { AlertCircle } from "lucide-react";
 import React from "react";
+import { AlertDialogHeader } from "../ui/alert-dialog";
 
-interface ConfirmM
-odalProps {
+interface ConfirmModalProps {
   children: React.ReactNode;
   onConfirm: () => void;
 }
 
-const ConfirmModal = () => {
-  return <div>ConfirmModal</div>;
+const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader><AL</AlertDialogHeader>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
 };
 
 export default ConfirmModal;
