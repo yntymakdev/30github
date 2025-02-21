f8c17bb99953a6370056e71d795aebd6b24cf698
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 19:20:25 +0600

    ok custmoize bunner

--- src/components/bunner.tsx
+++ src/components/bunner.tsx
@@ -1,6 +1,7 @@
+import { cva } from "class-variance-authority";
 import React from "react";
 
-const bannerVariants = cva("bord");
+const bannerVariants = cva("bord text-center p-4 text-sm flex items-center w-full");
 
 const Banner = () => {
   return <div>Banner</div>;
