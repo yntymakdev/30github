22352a5e443134315abe127faa7071dc6d312d3b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 15:15:13 +0600

    fixed bug on page courseId

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,106 +9,106 @@ import { ImageForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses
 import { CategoryForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/CategoryForm";
 import { PriceForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/PriceForm";
 import { AttachmentForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/AttachmentForm";
-import {ChapterForm} from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm";
+import { ChapterForm } from "@/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/ChapterForm";
 
-const CourseIdPagePage = async ({ params }: { params: { courseId: string } }) => {
-    const { userId } = await auth();
-    if (!userId) {
-        return redirect("/");
-    }
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
 
-    const course = await db.course.findUnique({
-        where: { id: params.courseId },
-        include: {
-            chapters:{
-                orderBy:{
-                    position: 'asc'
-                }
-            },
-            attachments: {
-                orderBy: {
-                    createdAt: "desc",
-                },
-            },
+  const course = await db.course.findUnique({
+    where: { id: params.courseId },
+    include: {
+      chapters: {
+        orderBy: {
+          position: "asc",
         },
-    });
+      },
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
 
-    const categories = await db.category.findMany({ orderBy: { name: "asc" } });
+  const categories = await db.category.findMany({ orderBy: { name: "asc" } });
 
-    if (!course) {
-        return redirect("/");
-    }
+  if (!course) {
+    return redirect("/");
+  }
 
-    const requiredFields = [
-        course.title,
-        course.description,
-        course.imageUrl,
-        course.price,
-        course.categoryId,
-        course.chapters.some(chapter => chapter.isPublished)     ,
-    ];
-    const totalFields = requiredFields.length;
-    const completedFields = requiredFields.filter(Boolean).length;
-    const completionText = `(${completedFields}/${totalFields})`;
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
+  ];
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
 
-    return (
-        <div className="p-6">
-            <div className="flex items-center justify-between">
-                <div className="flex flex-col gap-y-2">
-                    <h1 className="text-2xl font-medium">Course Setup</h1>
-                    <span className="text-sm text-slate-700">Completed fields {completionText}</span>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={LayoutDashboard} />
-                        <h2 className="text-xl">Customize your course</h2>
-                    </div>
-
-                    <TitleForm initialData={{ title: course.title ?? "" }} courseId={course.id} />
-                    <DescriptionForm initialData={course} courseId={course.id} />
-                    <ImageForm initialData={course} courseId={course.id} />
-                    <CategoryForm
-                        initialData={course}
-                        courseId={course.id}
-                        options={categories.map((category) => ({
-                            label: category.name,
-                            value: category.id,
-                        }))}
-                    />
-                </div>
-
-                <div className="space-y-6">
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge icon={ListChecks} />
-                            <h2 className="text-xl">Course chapters</h2>
-                        </div>
-                        <ChapterForm initialData={course} courseId={course.id} />
-                        <p>TODO: Chapters</p>
-                    </div>
-
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge icon={CircleDollarSign} />
-                            <h2 className="text-xl">Sell your course</h2>
-                        </div>
-                        <PriceForm initialData={course} courseId={course.id} />
-                    </div>
-
-                    <div>
-                        <div className="flex items-center gap-x-2">
-                            <IconBadge icon={File} />
-                            <h2 className="text-xl">Resource Attachment</h2>
-                        </div>
-                        <AttachmentForm initialData={course} courseId={course.id} />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">Completed fields {completionText}</span>
         </div>
-    );
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+
+          <TitleForm initialData={{ title: course.title ?? "" }} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
+        </div>
+
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListChecks} />
+              <h2 className="text-xl">Course chapters</h2>
+            </div>
+            <ChapterForm initialData={course} courseId={course.id} />
+            <p>TODO: Chapters</p>
+          </div>
+
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSign} />
+              <h2 className="text-xl">Sell your course</h2>
+            </div>
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
+
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Resource Attachment</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default CourseIdPagePage;
+export default CourseIdPage;
