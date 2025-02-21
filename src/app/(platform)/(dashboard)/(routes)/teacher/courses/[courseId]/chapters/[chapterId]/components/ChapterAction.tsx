d0d179e59de23ac4f7ba0afab0e5cb1c07975fb7
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-21 21:00:48 +0600

    chapter actions creating

--- src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/components/ChapterAction.tsx
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/components/ChapterAction.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
 
+interface ChapterActionProps {
+  disabled: boolean;
+  courseId: boolean;
+  chapterId: boolean;
+  disabled: boolean;
+}
+
 const ChapterAction = () => {
   return <div>ChapterAction</div>;
 };
