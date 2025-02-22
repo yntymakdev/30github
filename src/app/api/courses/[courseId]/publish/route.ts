3be1bd988bba3513ae260520ad60bc69b21a0c04
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-22 16:10:18 +0600

    customized publish route ts on courseid api

--- src/app/api/courses/[courseId]/publish/route.ts
+++ src/app/api/courses/[courseId]/publish/route.ts
@@ -35,12 +35,15 @@ export async function PATCH(req: Request, { params }: { params: { courseId: stri
 
     const hasPublishedChapter = course.chapters.some((chapter) => chapter.isPublished);
 
-    if (!course.title || !course.description || !course.imageUrl || !course.categoryId || !hasPublishedChapter)
-      const deletedCourse = await db.course.delete({
-        where: {
-          id: params.courseId,
-        },
-      });
+    if (!course.title || !course.description || !course.imageUrl || !course.categoryId || !hasPublishedChapter) {
+      return new NextResponse("Missing required  fields", { status: 401 });
+    }
+    const publishedCourse = await db.course.update({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
 
     return NextResponse.json(deletedCourse);
   } catch (error) {
