920737dd43f29114ae2efda9c995ab56de46c385
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-22 20:35:40 +0600

    biig work

--- src/app/(platform)/(dashboard)/(routes)/search/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/search/page.tsx
@@ -1,11 +1,19 @@
-'use client'
-export default function SearchPage ()  {
+"use client";
+
+import { db } from "@/lib/db";
+import Categories from "./components/Categories/Categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
   return (
     <div>
-    SearchPage 
-  
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
     </div>
   );
 };
-
-
