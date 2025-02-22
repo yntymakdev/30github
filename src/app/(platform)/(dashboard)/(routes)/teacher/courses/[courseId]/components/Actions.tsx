70f96de53bdb13b577cf1195dd33818c2bb8b12e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-22 15:25:51 +0600

    customized actions page on chapter

---
+++ src/app/(platform)/(dashboard)/(routes)/teacher/courses/[courseId]/components/Actions.tsx
@@ -0,0 +1,67 @@
+"use client";
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { AArrowDown, Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+interface ActionsProps {
+  disabled: boolean;
+  courseId: string;
+  isPublished: boolean | null;
+}
+
+const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/unpublish`);
+        toast.success("Course unpublish");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/publish`);
+        toast.success("Course published");
+      }
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}`);
+      toast.success("Course  deleted!");
+      router.refresh();
+      router.push(`/teacher/courses`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button onClick={onClick} disabled={disabled || isLoading} variant="outline" size="sm">
+        {isPublished ? "Unpublished" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default Actions;
