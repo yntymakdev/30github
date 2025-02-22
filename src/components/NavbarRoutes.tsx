be4c0ff1b470b4d32c072cd8b03d96498fa4b15d
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-22 21:50:33 +0600

    finished some page

--- src/components/NavbarRoutes.tsx
+++ src/components/NavbarRoutes.tsx
@@ -1,38 +1,43 @@
-'use client'
-import {UserButton} from "@clerk/nextjs";
-import {usePathname, useRouter} from "next/navigation";
-import {Button} from "@/components/ui/button";
-import {LogOut} from "lucide-react";
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 import Link from "next/link";
+import SearchInput from "./SearchInput";
 
-export default function NavbarRoutes ()  {
-  const  pathname = usePathname();
+export default function NavbarRoutes() {
+  const pathname = usePathname();
   const router = useRouter();
 
   const isTeacherPage = pathname.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
-
+  const isSearchPage = pathname === "/search";
   return (
-    <div className='flex gap-x-2 ml-auto'>
-      {
-        isTeacherPage || isPlayerPage ? (
-            <Link href='/'>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
 
-            <Button size='sm' variant='ghost'>
-              <LogOut className='h-4 w-4 mr-2'/>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
-              </Link>
-        ) :(
-            <Link href='/teacher/courses'>
-              <Button size='sm' variant='ghost'>
-                Teach Mode
-              </Button>
-            </Link>
-      )}
-      <UserButton/>
-    </div>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teach Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
-};
-
-
+}
