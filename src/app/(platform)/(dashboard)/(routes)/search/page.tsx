88c4112e8c7b9d5603d70910bfa5b5a40579ae44
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-23 21:35:19 +0600

    fized bug.....

--- src/app/(platform)/(dashboard)/(routes)/search/page.tsx
+++ src/app/(platform)/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,18 @@
 import { db } from "@/lib/db";
 import Categories from "./components/Categories/Categories";
 import SearchInput from "@/components/SearchInput";
+import { getCourses } from "../../../../../../actions/getCourses";
+import { auth } from "@clerk/nextjs/server";
 
 const SearchPage = async () => {
+  const { userId } = await auth();
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+  const courses = await getCourses;
+
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
