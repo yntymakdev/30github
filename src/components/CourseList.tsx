fb614e05fc8dc45c958add95a5760052af2786c3
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 13:57:03 +0600

    fixed big that is it

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Category, Course } from "@prisma/client";
+import { Item } from "@radix-ui/react-dropdown-menu";
 import React from "react";
 
 type CourseWithProgressWithCategory = Course & {
@@ -19,13 +20,25 @@ export const CourseList = ({ items }: CourseListProps) => {
     <div>
       <div className="grid sm:grid-cols-2 md:grid-cols-2 lg-grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
         {items.map((item) => (
-          <CourseCard />
+          <CourseCard 
+          key ={item.id }
+id={item.id}
+title={item.title}
+imageUrl = {item.imageUrl}
+chaptersLength={item.chapters.length}
+price={item.price}
+progress={item.progress}
+category={item?.category?.name}
+          />
         ))}
       </div>
       <div>
         {items.length=== 0 && (
-          
-        )          <CourseCard />
+          <div className="text-center text-sm  text-muted-foreground mt-10">
+Not course Found
+        
+          </div>
+        ) 
         ))}
       </div>
     </div>
