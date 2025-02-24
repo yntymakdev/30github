e0237b9eea4c6175125929503557e867b3e168e9
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 14:15:40 +0600

    customize now coursecard...

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -1,4 +1,5 @@
 import { Course } from "@prisma/client";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -16,7 +17,11 @@ export const CourseCard = ({ id, title, price, imageUrl, chaptersLength, progres
   return (
     <div>
       <Link href={`/courses/${id}`}>
-        <div className="group  hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full"></div>
+        <div className="group  hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+          <div className="relative w-full aspect-video rounded-md overflow-hidden">
+            <Image fill className="object-cover" src={imageUrl} alt="title" />
+          </div>
+        </div>
       </Link>
     </div>
   );
