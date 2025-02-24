44cd8ae7b1c932ca233081188b986fbf0324dfb2
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 13:48:53 +0600

    fixed big that is it

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -17,12 +17,17 @@ export const CourseList = ({ items }: CourseListProps) => {
 
   return (
     <div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg-grid-cols-3">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg-grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
         {items.map((item) => (
-          <div key={item.id}>{item.title}</div>
+          <CourseCard />
         ))}
       </div>
-      <div></div>
+      <div>
+        {items.length=== 0 && (
+          
+        )          <CourseCard />
+        ))}
+      </div>
     </div>
   );
 };
