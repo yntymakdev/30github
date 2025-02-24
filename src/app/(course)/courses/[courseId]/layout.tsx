befaa9f0d44b5f4083d74061f99bdffff6378ce8
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 15:55:33 +0600

    customize layout page for course page courseId

--- src/app/(course)/courses/[courseId]/layout.tsx
+++ src/app/(course)/courses/[courseId]/layout.tsx
@@ -1,6 +1,15 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CourseLayout = ({ children }: { children: React.ReactNode }) => {
+const CourseLayout = async ({ children }: { children: React.ReactNode }) => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({ where: { id: params.courseId } });
+
   return <div>{children}</div>;
 };
 
