76683ebd2e07913963a6299cb38125881485b0d1
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 14:06:40 +0600

    customize now coursecard...

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Category, Course } from "@prisma/client";
-import { Item } from "@radix-ui/react-dropdown-menu";
 import React from "react";
 import { CourseCard } from "./CourseCard";
 
@@ -19,29 +18,22 @@ export const CourseList = ({ items }: CourseListProps) => {
 
   return (
     <div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg-grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
         {items.map((item) => (
-          <CourseCard 
-          key ={item.id }
-id={item.id}
-title={item.title || "no title"}
-imageUrl = {item.imageUrl!}
-chaptersLength={item.chapters.length}
-price={item.price!}
-progress={item.progress}
-category={item?.category?.name!}
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl}
+            chaptersLength={item.chapters.length}
+            price={item.price ?? 0}
+            progress={item.progress}
+            category={item?.category?.name!}
           />
         ))}
       </div>
-      <div>
-        {items.length=== 0 && (
-          <div className="text-center text-sm  text-muted-foreground mt-10">
-Not course Found
-        
-          </div>
-        ) 
-        )}
-      </div>
+
+      {items.length === 0 && <div className="text-center text-sm text-muted-foreground mt-10">No courses found</div>}
     </div>
   );
 };
