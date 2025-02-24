b0fad205200b1f03faf4b431c74f7f788fe33727
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-24 13:58:04 +0600

    customize now coursecard...

---
+++ src/components/CourseCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface CourseCardProps{
+id: string
+title: string
+imageUrl: string
+chaptersLength: number
+progress: number | null
+category: string
+}
+
+export  const CourseCard = () => {
+  return (
+    <div>CourseCard</div>
+  )
