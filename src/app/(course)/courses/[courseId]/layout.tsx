3f54cece7f9da124319219018b56bfe3962494dc
yntymakdev <kubanychevyntymak21@gmail.com>
2025-02-26 11:21:18 +0600

    now starting again

--- src/app/(course)/courses/[courseId]/layout.tsx
+++ src/app/(course)/courses/[courseId]/layout.tsx
@@ -3,46 +3,45 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 import { getProgress } from "../../../../../actions/getProgress";
+import CourseSideBar from "@/components/CourseSideBar";
 
-const CourseLayout = async ({ children }: { children: React.ReactNode }) => {
+const CourseLayout = async ({ children, params }: { children: React.ReactNode; params: { courseId: string } }) => {
   const { userId } = await auth();
   if (!userId) {
-    return redirect("/");
+    redirect("/");
+    return null; // Чтобы избежать ошибки, так как redirect не возвращает JSX
   }
-  const course = await db.course.findUnique({ where: { id: params.courseId },
-  
-  }include:{
-    chapters: {
-      where: {
-        isPublished: true
+
+  const course = await db.course.findUnique({
+    where: { id: params.courseId },
+    include: {
+      chapters: {
+        where: { isPublished: true },
+        include: {
+          userProgress: {
+            where: { userId },
+          },
+        },
+        orderBy: { position: "asc" },
       },
-      include: {
-        userPorgress: {
-          where:{
-            userId
-          }
-        }
-      },
-      orderBy: {
-        position: "asc"
-      }
-    }
-  }
+    },
   });
 
-if(!course){
-  redirect('/')
-}
+  if (!course) {
+    redirect("/");
+    return null;
+  }
 
-const porgresCount = await getProgress(userId,course.id)
+  const progressCount = await getProgress(userId, course.id);
 
-  return <div className="h-full">
-  <div className="hidden md:flex-full w-full flex-col fixed inset-y-0 z-50">
-    <CourseSideBar/>
-  </div>
-    <main>
-    {children}
-  </main>
-    </div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex-full w-full flex-col fixed inset-y-0 z-50">
+        <CourseSideBar />
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
 
 export default CourseLayout;
